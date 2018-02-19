3f69891aec440bce9986d2126617ba367892cf51
ShrubSkrub <sean.duarte@gmail.com>
2018-02-18 21:30:26 -0800

    removed .swp and switch tabs for spaces

--- compiletesting/convert.cpp
+++ compiletesting/convert.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-	double ctemp, ftemp;
-	cout << "Input a Celsius temp and press Enter: ";
-		cin >> ctemp;
-		ftemp = (ctemp * 1.8) + 32;
-	cout << "Farenheit temp is: " << ftemp;
-	return 0;
+    double ctemp, ftemp;
+    cout << "Input a Celsius temp and press Enter: ";
+        cin >> ctemp;
+        ftemp = (ctemp * 1.8) + 32;
+    cout << "Farenheit temp is: " << ftemp;
+    return 0;
 }
