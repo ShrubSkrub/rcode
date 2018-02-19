cc0a790b9d8b8a8827ec3ca7f3dc1e5d114636e8
ShrubSkrub <sean.duarte@gmail.com>
2018-02-18 20:53:03 -0800

    Update helloworld.cpp

--- cis118/helloworld.cpp
+++ cis118/helloworld.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-using namespace std;
-
-int main()
+#include <iostream>  //Name of prewritten code <file> you want to use…
+                     //Good programmers REUSE existing code
+                     //#includes names files/libraries of existing code you want to reuse
+using namespace std; //Command to avoid typing std:: in front of cout and endl commands
+int main( )          //All programs start at ‘main’ – simple version of main, others possible
 {
-    cout<<"Hello World"<<endl<<endl;
-
-    // system ("Pause");
-    return 0;
+cout << "Hello World"<< endl << endl; // cout command to print “message” to monitor
+system ("Pause");    // Command needed to pause and see output on PC… remove this line on Mac
+return 0;            // End program
 }
