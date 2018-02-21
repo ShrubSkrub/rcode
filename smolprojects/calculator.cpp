b7aae4368b1f1cbacba166f7bf6eea34ed783ae2
ShrubSkrub <sean.duarte@gmail.com>
2018-02-21 11:57:57 -0800

    Removed spaces message

--- smolprojects/calculator.cpp
+++ smolprojects/calculator.cpp
@@ -6,7 +6,7 @@ int main()
     double n1, n2, output;
     char op;
 
-    cout << "Input operation, seperated by spaces: ";
+    cout << "Input operation: ";
     cin >> n1 >> op >> n2;
 
     switch (op)
