55e260e097fe54fbd6b72476b11ce2b200fd5bea
ShrubSkrub <sean.duarte@gmail.com>
2018-02-28 21:39:37 -0800

    Added cis assignment

---
+++ cis118/w7A1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>   // Added include and <
+using namespace std;
+
+int main ()  // start program  //Changed I to i
+{   // Added {
+
+    //Declare and Initialize Variables
+    int height = 0;
+    int feet = 0;   //Added ;
+    int inches = 0;     //Changed I to i
+
+    // Prompt for height
+    cout << "Enter your height in inches: ";    //Changed >> to <<
+    cin >> height;  //Changed H to h
+
+    // Calculate Height in feet and inches
+    feet = height / 12 ;    //Switch order so feet is in front
+    inches = height % 12;
+
+    //Print out height in feet and inches   //Added /
+    cout << "You are " << feet << " feet and " << inches << "inches";   //Removed t, changed FEET to feet, added <, changed <> to <<, changed >> to <<
+
+    // system("pause");  // Mac user comment out this line by placing // in front of it.  
+    // PC users -Hint replace the " with the ones on your computer--coping code cause error, so just type it.
+    return 0;
+
+} // end of program
---
+++ cis118/w7A1Unfixed.cpp
@@ -0,0 +1,24 @@
+#  iostream>
+using namespace std;
+
+Int main ( )  // start program
+    /Declare and Initialize Variables
+    int height = 0;
+    int feet = 0
+    Int inches = 0;
+
+    // Prompt for height
+    cout >> "Enter your height in inches: ";
+    cin >> Height;
+
+    // Calculate Height in feet and inches
+    height / 12 = feet;
+    inches = height % 12;
+
+    /Print out height in feet and inches
+    coutt << "You are " < FEET << " feet and " <> inches >> "inches" ;
+
+    system("pause");  // Mac user comment out this line by placing // in front of it.  
+    // PC users -Hint replace the " with the ones on your computer--coping code cause error, so just type it.
+    return 0;
+} // end of program
