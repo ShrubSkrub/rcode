566874f800c31e69e10aa2b3a4fb7d8258d379eb
ShrubSkrub <sean.duarte@gmail.com>
2018-03-01 22:14:17 -0800

    Added height program for cis

---
+++ cis118/heightW7A2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Sean Duarte - 2/24/18" << endl;  // Name and date to show this is my code
+    int height = 0, feet = 0, inches = 0;
+    cout << "How many feet and how many inches tall are you?" << endl;
+    cin >> feet >> inches;
+    height = feet * 12 + inches;
+
+    cout << "You are " << height << " inches tall." << endl;
+    return 0;  // End program
+}
