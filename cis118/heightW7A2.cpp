a7fd15cbc29a6e4267b26c14497cb6037ca05592
ShrubSkrub <sean.duarte@gmail.com>
2018-03-04 11:15:21 -0800

    Cleaned up code

--- cis118/heightW7A2.cpp
+++ cis118/heightW7A2.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Sean Duarte - 2/24/18"
-         << endl;  // Name and date to show this is my code
-    int height = 0, feet = 0, inches = 0;
-    cout << "How many feet and how many inches tall are you?"
-         << endl;  // Ask user for input
-    cin >> feet >> inches;
-    height = feet * 12 + inches;
+    // Name and date to show this is my code
+    cout << "Sean Duarte - 2/24/18" << endl;
 
-    cout << "You are " << height << " inches tall."
-         << endl;  // Send user output
-    return 0;      // End program
+    // Declare variables
+    int height = 0, feet = 0, inches = 0;
+    // Take input
+    cout << "How many feet and how many inches tall are you?" << endl;
+    cin >> feet >> inches;
+    // Preform operation
+    height = feet * 12 + inches;
+    // Send output
+    cout << "You are " << height << " inches tall." << endl;
+
+    // End program
+    return 0;
 }
