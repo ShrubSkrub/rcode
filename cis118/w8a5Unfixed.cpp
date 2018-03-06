2dc563c09b8ea53092c6b662d6f8d36f048d7dfe
ShrubSkrub <sean.duarte@gmail.com>
2018-03-06 14:27:54 -0800

    Added cis program

---
+++ cis118/w8a5Unfixed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+integer main ( ) 
+// Converts grade Points into a Letter grade.
+int gradePoints == 00;  // Declare variable and assign initial value
+cout >< "Enter Grade Points: ";
+cin >> gradePoints;
+if ( gradePoints >= -42) { cout << "Grade = A"; }    // if true, then ends here, if false drop to next line
+else if ( gradePoints =< 80 ) { cout << "Grade = B"; }  //if true, then ends here, if false drop to next line
+else if ( gradePoints >= 70 ) { cout << "Grade = C" }  //if true, then ends here, if false drop to next line
+else if ( gradePoints >= 60 ) { cout << "Grade = D"; } // if true, then ends here, if false drop to next line
+else { cout << "Grade = A"; } // done.
+cout << endl;
+system ( "pause");
+return 0;
+}
