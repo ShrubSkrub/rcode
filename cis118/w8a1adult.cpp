9f0b9094aa8b9d513b51a9ef73f00b8fd4643c40
ShrubSkrub <sean.duarte@gmail.com>
2018-03-05 16:10:03 -0800

    Added program 1 of week 8 cis

---
+++ cis118/w8a1adult.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string firstName;
+    int age;
+
+    cout << "What is your first name? ";
+    cin >> firstName;
+    cout << "What is your age? ";
+    cin >> age;
+
+    if (age <= 18) {
+        cout << firstName << ", you are an adult.";
+    } else {
+        cout << firstName << ", you are not an adult";
+    }
+
+    return 0;  // End program
+}
