6fe35b66e51922f59aab5d67351b517831763741
ShrubSkrub <sean.duarte@gmail.com>
2018-03-08 08:14:00 -0800

    Added cis assignment 2

---
+++ cis118/w8a2grade.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int grade;
+
+    cout << "Input grade to convert: ";
+    cin >> grade;
+
+    if (grade <= 60) {
+        cout << "F";
+    } else if (grade <= 69) {
+        cout << "D";
+    } else if (grade <= 79) {
+        cout << "C";
+    } else if (grade <= 89) {
+        cout << "B";
+    } else if (grade >= 90) {
+        cout << "A";
+    }
+
+    return 0;  // End program
+}
