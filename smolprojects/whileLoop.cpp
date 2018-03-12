1526e6f36d9169129e8dbbc3a671af9545f2be92
ShrubSkrub <sean.duarte@gmail.com>
2018-03-12 10:42:21 -0700

    Added some programs

---
+++ smolprojects/whileLoop.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main() {
+    unsigned int i = 2;
+    while (true) {
+        cout << i << endl;
+        i = pow(i, 2);
+        cout << i << endl;
+        // system("pause");
+    }
+
+    return 0;  // End program
+}
---
+++ smolprojects/whileLoop.py
@@ -0,0 +1,6 @@
+import time
+i = 2
+while True:
+    i = i**2
+    print(str(i) + " ")
+    time.sleep(1)
