1cefc7f9aec4356366053e5801c1b2cfc0df24a4
ShrubSkrub <sean.duarte@gmail.com>
2018-07-18 21:34:49 -0700

    Added multiple new functions to game

--- cis250/w6/memorymatch.cpp
+++ cis250/w6/memorymatch.cpp
@@ -8,34 +8,61 @@ using namespace std;
 
 class MemoryMatchGame{
     protected:
-        string cards[50] = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-                            "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
-                            "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
-                            "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
+        string cards[50] = {"H ", "He", "Li", "Be", "B ", "C ", "N ", "O ", "F ", "Ne",
+                            "Na", "Mg", "Al", "Si", "P ", "S ", "Cl", "Ar", "K ", "Ca",
+                            "Sc", "Ti", "V ", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+                            "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y ", "Zr",
                             "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn"};
         string face = "E";  // Theme
         int diff = 0;  // Difficulty
         int time = 0;  // Game speed
-        string *answerArr;
-        int *displayArr;
+        string answerArr[8][8];  // Holds answers
+        string displayArr[8][8];  // What is shown to the user
+        // string *answerArr;
+        // int *displayArr;
         int input;  // Temp for all needed cin
-        int arrSize = 0;
+        int arrSize = 0;  // Set during difficulty, limits function which act on array
+        bool gameRunning = true;  // Used to keep game running, or stop it
+        int pairs = 0;  // Tracks how many pairs have been found
     public:
         MemoryMatchGame(){
-            answerArr = new string[8][8];
+            // Fill arrays
+            for (int i = 0; i < input; i++){
+                for (int j = 0; j < input; j++){
+                    answerArr[i][j] = "  ";
+                    displayArr[i][j] = "E ";
+                }
+            }
         }
         void difficulty(){
             cout << "Choose difficulty:\n4 x 4 grid (Easy)\n6 x 6 grid (Moderate)\n";
-            cout << "8 x 8 grid (Difficult)\n";
-            cin >> input;
-            // Add check for valid input
-            // string answerArr[input][input] = { };
-            // string displayArr[input][input] = { face };
-            // TODO Make dynamic array belonging to class
-
+            cout << "8 x 8 grid (Difficult)\n    - Input 4, 6, or 8 -\n";
+            // Check for valid input
+            bool temp1 = true;
+            while (temp1) {
+                cin >> input;
+                switch (input) {
+                    case 8:
+                        pairs = 32;
+                        temp1 = false;
+                        break;
+                    case 6:
+                        pairs = 18;
+                        temp1 = false;
+                        break;
+                    case 4:
+                        pairs = 8;
+                        temp1 = false;
+                        break;
+                    default:
+                        cout << "Not a valid option. Try again.\n";
+                        break;
+                }
+            }
             arrSize = input;
         }
         void dealCards(){
+            cout << "Dealing cards...\n";
             // Create array's size
             // Shuffle cards array
             for (int i = 0; i < 50; i++){
@@ -45,7 +72,7 @@ class MemoryMatchGame{
                 cards[r] = temp;
             }
 
-            // Deal CARDS INTo answer array
+            // Deal CARDS into answer array
             int cardCount = 0;
             for (int i = 0; i < input; i++){
                 for (int j = 0; j < input; j++){
@@ -56,57 +83,151 @@ class MemoryMatchGame{
         }
         void speed(){
             cout << "Choose time interval:\n6 secs (Easy)\n4 secs (Moderate)\n";
-            cout << "2 secs (Difficult)\n";
-            cin >> input;
-            // Add check for valid input
+            cout << "2 secs (Difficult)\n    - Input 6, 4, or 2 -\n";
+            // Check for valid input
+            bool temp1 = true;
+            while (temp1) {
+                cin >> input;
+                switch (input) {
+                    case 6:
+                    case 4:
+                    case 2:
+                        temp1 = false;
+                        break;
+                    default:
+                        cout << "Not a valid option. Try again.\n";
+                        break;
+                }
+            }
             time = input;
         }
-        void display(){
-        }
+        // void display(){
+        // }
+        // bool inputCheck(string in){
+        // }
 
 
 
         void start(){
+            system("clear");
+            cout << "Welcome to Memory Match!\n\n";
+            difficulty();
+            cout << "\n====================\n";
+            speed();
+            cout << "\n====================\n";
+            system("clear");
+
+            dealCards();
+
+            while (gameRunning) {
+                // TODO Fix printing, possibly fault of array filling?
+                printArr();
+                // User selects square1
+                // User selects square2
+                // Compare squares
+
+                // Check if all squares have been found
+            }
         }
 
-
+// ----------------------------------------------------------------------
 
         // Old, possibly useful functions
-        void printArr(int arr[8][8]) {
-            cout << "\n[ ][1][2][3][4][5][6][7][8]\n";
-            for (int i = 0; i < 8; i++) {
-                switch (i) {
-                    case 0:
-                        cout << "[A]";
-                        break;
-                    case 1:
-                        cout << "[B]";
-                        break;
-                    case 2:
-                        cout << "[C]";
-                        break;
-                    case 3:
-                        cout << "[D]";
-                        break;
-                    case 4:
-                        cout << "[E]";
-                        break;
-                    case 5:
-                        cout << "[F]";
-                        break;
-                    case 6:
-                        cout << "[G]";
-                        break;
-                    case 7:
-                        cout << "[H]";
-                        break;
-                }
-                for (int j = 0; j < 8; j++) {
-                    cout << "[";
-                    cout << arr[i][j];
-                    cout << "]";
-                }
-                cout << endl;
+        void printArr() {
+            switch (arrSize) {
+                case 8:
+                    cout << "\n[ ][1][2][3][4][5][6][7][8]\n";
+                    for (int i = 0; i < 8; i++) {
+                        switch (i) {
+                            case 0:
+                                cout << "[A]";
+                                break;
+                            case 1:
+                                cout << "[B]";
+                                break;
+                            case 2:
+                                cout << "[C]";
+                                break;
+                            case 3:
+                                cout << "[D]";
+                                break;
+                            case 4:
+                                cout << "[E]";
+                                break;
+                            case 5:
+                                cout << "[F]";
+                                break;
+                            case 6:
+                                cout << "[G]";
+                                break;
+                            case 7:
+                                cout << "[H]";
+                                break;
+                        }
+                        for (int j = 0; j < 8; j++) {
+                            cout << "[";
+                            cout << displayArr[i][j];
+                            cout << "]";
+                        }
+                        cout << endl;
+                    }
+                    break;
+                case 6:
+                    cout << "\n[ ][1][2][3][4][5][6]\n";
+                    for (int i = 0; i < 6; i++) {
+                        switch (i) {
+                            case 0:
+                                cout << "[A]";
+                                break;
+                            case 1:
+                                cout << "[B]";
+                                break;
+                            case 2:
+                                cout << "[C]";
+                                break;
+                            case 3:
+                                cout << "[D]";
+                                break;
+                            case 4:
+                                cout << "[E]";
+                                break;
+                            case 5:
+                                cout << "[F]";
+                                break;
+                        }
+                        for (int j = 0; j < 6; j++) {
+                            cout << "[";
+                            cout << displayArr[i][j];
+                            cout << "]";
+                        }
+                        cout << endl;
+                    }
+                    break;
+                case 4:
+                    cout << "\n[ ][1][2][3][4]\n";
+                    for (int i = 0; i < 4; i++) {
+                        switch (i) {
+                            case 0:
+                                cout << "[A]";
+                                break;
+                            case 1:
+                                cout << "[B]";
+                                break;
+                            case 2:
+                                cout << "[C]";
+                                break;
+                            case 3:
+                                cout << "[D]";
+                                break;
+                        }
+                        for (int j = 0; j < 4; j++) {
+                            cout << "[";
+                            cout << displayArr[i][j];
+                            cout << "]";
+                        }
+                        cout << endl;
+                    }
+                    break;
             }
         }
 
@@ -173,7 +294,7 @@ class MemoryMatchGame{
                     }
                     arr[letterNum][number - 1] = 1;
                 }
-                printArr(arr);
+                printArr();
                 cout << "Is this correct? (y or n): ";
                 while (true) {
                     cin >> yn;
