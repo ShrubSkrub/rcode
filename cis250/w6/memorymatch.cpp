c022b35e3618b5cecf71baf9a5e9ddf76bb0ef59
ShrubSkrub <sean.duarte@gmail.com>
2018-07-19 23:56:56 -0700

    Practically done with final, just needs cleanup and possibly one hotfix

--- cis250/w6/memorymatch.cpp
+++ cis250/w6/memorymatch.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 #include <cstdlib>
 #include <ctime>
+#include <time.h>
+#include <unistd.h>
 
 class MemoryMatchGame{
     protected:
@@ -13,26 +15,18 @@ class MemoryMatchGame{
                             "Sc", "Ti", "V ", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
                             "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y ", "Zr",
                             "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn"};
-        string face = "E";  // Theme
+        string face = "E ";  // Theme
         int diff = 0;  // Difficulty
-        int time = 0;  // Game speed
+        int gameSpeed = 0;  // Game speed
         string answerArr[8][8];  // Holds answers
         string displayArr[8][8];  // What is shown to the user
-        // string *answerArr;
-        // int *displayArr;
         int input;  // Temp for all needed cin
         int arrSize = 0;  // Set during difficulty, limits function which act on array
+        int neededTerms = 0;  // Used in setting array size, is also number of pairs needed
         bool gameRunning = true;  // Used to keep game running, or stop it
         int pairs = 0;  // Tracks how many pairs have been found
     public:
         MemoryMatchGame(){
-            // Fill arrays
-            for (int i = 0; i < input; i++){
-                for (int j = 0; j < input; j++){
-                    answerArr[i][j] = "  ";
-                    displayArr[i][j] = "E ";
-                }
-            }
         }
         void difficulty(){
             cout << "Choose difficulty:\n4 x 4 grid (Easy)\n6 x 6 grid (Moderate)\n";
@@ -45,14 +39,17 @@ class MemoryMatchGame{
                     case 8:
                         pairs = 32;
                         temp1 = false;
+                        neededTerms = 32;
                         break;
                     case 6:
                         pairs = 18;
                         temp1 = false;
+                        neededTerms = 18;
                         break;
                     case 4:
                         pairs = 8;
                         temp1 = false;
+                        neededTerms = 8;
                         break;
                     default:
                         cout << "Not a valid option. Try again.\n";
@@ -60,34 +57,81 @@ class MemoryMatchGame{
                 }
             }
             arrSize = input;
+
+            // Fill arrays based on input from user
+            for (int i = 0; i < input; i++){
+                cout << "i = " << i << endl;
+                for (int j = 0; j < input; j++){
+                    cout << "j = " << j << endl;
+                    answerArr[i][j] = "  ";
+                    displayArr[i][j] = "E ";
+                    cout << "displayArr[" << i << "][" << j << "] = " << displayArr[i][j] << endl;
+                }
+            }
         }
         void dealCards(){
             cout << "Dealing cards...\n";
-            // Create array's size
             // Shuffle cards array
+            cout << "Shuffling cards array...\n";
             for (int i = 0; i < 50; i++){
                 int r = rand() % 50;
+                cout << "r = " << r << endl;
                 string temp = cards[i];
+                cout << "string temp = " << temp << endl;
                 cards[i] = cards[r];
                 cards[r] = temp;
+                cout << "cards[" << i << "] = " << cards[i] << endl;
+                cout << "cards[" << r << "] = " << cards[r] << endl;
+                cout << "temp = " << temp << endl;
             }
 
             // Deal CARDS into temp array
-            // Needs cards to be input twice
-            int cardCount = 0;
-            string tempArr[(input * 2)][(input * 2)];
-            for (int i = 0; i < (input * 2); i++){
-                for (int j = 0; j < (input * 2); j++){
-                    tempArr[i][j] = cards[cardCount];
-                    cardCount++;
-                    tempArr[i][j++] = cards[cardCount];
-                    cardCount++;
-                }
+            // Cards need to be input twice
+            cout << "Dealing pairs into tempArr..." << endl;
+            int cardCount = 0;  // Kind of like the number of the card or INDEX?
+            // cout << "input = " << input << endl;
+            string tempArr[(neededTerms * 2)];
+
+            for (int i = 0; i < (neededTerms * 2); i++){
+                    // cout << "cardCount = " << cardCount << endl;
+                tempArr[i] = cards[cardCount];
+                    // cout << "tempArr[" << i << "] = " << tempArr[i] << endl;
+                // cardCount++;
+                    // cout << "cardCount = " << cardCount << endl;
+                i++;
+                tempArr[i] = cards[cardCount];
+                    // cout << "tempArr[" << i << "] = " << tempArr[i] << endl;
+                cardCount++;
+            }
+
+            // Shuffle temp array
+            cout << "Shuffling temp array...\n";
+            for (int i = 0; i < (neededTerms * 2); i++){
+                int r = rand() % input;
+                cout << "r = " << r << endl;
+                string temp = tempArr[i];
+                cout << "string temp = " << temp << endl;
+                tempArr[i] = tempArr[r];
+                tempArr[r] = temp;
+                cout << "tempArr[" << i << "] = " << tempArr[i] << endl;
+                cout << "tempArr[" << r << "] = " << tempArr[r] << endl;
+                cout << "temp = " << temp << endl;
             }
 
             // Deal CARDS into answer array
+            cardCount = 0;
+            for (int i = 0; i < input; i++){
+                // cout << "i = " << i << endl;
+                for (int j = 0; j < input; j++){
+                    // cout << "j = " << j << endl;
+                    answerArr[i][j] = tempArr[cardCount];
+                    cout << "answerArr[" << i << "][" << j << "] = " << answerArr[i][j] << endl;
+                    cardCount++;
+                }
+            }
         }
         void speed(){
+            system("clear");
             cout << "Choose time interval:\n6 secs (Easy)\n4 secs (Moderate)\n";
             cout << "2 secs (Difficult)\n    - Input 6, 4, or 2 -\n";
             // Check for valid input
@@ -105,38 +149,78 @@ class MemoryMatchGame{
                         break;
                 }
             }
-            time = input;
+            gameSpeed = input;
         }
 
-        void winCheck(){
+        void pickSquares(){
+            system("clear");
+            pairs = 0;
+            // cout << "Pairs = " << pairs << endl;
+            // cout << "neededTerms = " << neededTerms << endl;
+            char letter, letter2;
+            int number, letterNum, number2, letterNum2, loop = 0;
+            string pick1, pick2;
+            while (loop == 0) {
+                cout << "Pairs found: " << pairs << "/" << neededTerms << endl;
+                printArr();
+
+                // Select square 1
+                cout << "Enter first card (ie. H8): ";
+                cin >> letter >> number;
+                letterNum = convCharToNum(letter);
+                pick1 = answerArr[letterNum][number - 1];
+                displayArr[letterNum][number - 1] = answerArr[letterNum][number - 1];
+                printArr();
+
+                // Select square 2
+                cout << "Enter second card (ie. H8): ";
+                cin >> letter2 >> number2;
+                letterNum2 = convCharToNum(letter2);
+                pick2 = answerArr[letterNum2][number2 - 1];
+                displayArr[letterNum2][number2 - 1] = answerArr[letterNum2][number2 - 1];
+                printArr();
+
+                // Check for match
+                if (pick1 == pick2) {
+                    cout << "It's a match!\n";
+                    pairs++;
+                    if (pairs == neededTerms) {
+                        cout << "You win!\n";
+                        loop = 1;
+                    }
+                    sleep(2);
+                } else {
+                    cout << "No match...\n";
+                    displayArr[letterNum][number - 1] = "E ";
+                    displayArr[letterNum2][number2 - 1] = "E ";
+                    switch (gameSpeed) {
+                        case 6:
+                            sleep(6);
+                            break;
+                        case 4:
+                            sleep(4);
+                            break;
+                        case 2:
+                            sleep(2);
+                            break;
+                    }
+                }
+                system("clear");
+            }
         }
-        // void display(){
-        // }
-        // bool inputCheck(string in){
-        // }
-
-
 
         void start(){
             system("clear");
+            srand(time(0));
+            cout << rand() << endl;
             cout << "Welcome to Memory Match!\n\n";
             difficulty();
             cout << "\n====================\n";
             speed();
             cout << "\n====================\n";
             system("clear");
-
             dealCards();
-
-            while (gameRunning) {
-                // TODO Fix printing, possibly fault of array filling?
-                printArr();
-                // User selects square1
-                // User selects square2
-                // Compare squares
-
-                // Check if all squares have been found
-            }
+            pickSquares();
         }
 
 // ----------------------------------------------------------------------
@@ -145,7 +229,7 @@ class MemoryMatchGame{
         void printArr() {
             switch (arrSize) {
                 case 8:
-                    cout << "\n[ ][1][2][3][4][5][6][7][8]\n";
+                    cout << "\n[ ][1 ][2 ][3 ][4 ][5 ][6 ][7 ][8 ]\n";
                     for (int i = 0; i < 8; i++) {
                         switch (i) {
                             case 0:
@@ -182,7 +266,7 @@ class MemoryMatchGame{
                     }
                     break;
                 case 6:
-                    cout << "\n[ ][1][2][3][4][5][6]\n";
+                    cout << "\n[ ][1 ][2 ][3 ][4 ][5 ][6 ]\n";
                     for (int i = 0; i < 6; i++) {
                         switch (i) {
                             case 0:
@@ -213,7 +297,7 @@ class MemoryMatchGame{
                     }
                     break;
                 case 4:
-                    cout << "\n[ ][1][2][3][4]\n";
+                    cout << "\n[ ][1 ][2 ][3 ][4 ]\n";
                     for (int i = 0; i < 4; i++) {
                         switch (i) {
                             case 0:
